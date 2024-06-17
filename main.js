e2198d5c3c12aed219b37d844d96ebe17c74b0ff
9boJ <jatinbhagat7766@gmail.com>
2024-06-17 07:48:07 -0700

    ww

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) { return new bootstrap.Tooltip(tooltipTriggerEl)})s
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) { return new bootstrap.Tooltip(tooltipTriggerEl)})
+(function (document) {
+    "use strict";
+    const ready = (callback) => {
+      if (document.readyState != "loading") callback();
+      else document.addEventListener("DOMContentLoaded", callback);
+    };
+    ready(() => {
+      const img = document.getElementById("image");
+      const simpleModal = document.getElementById("simple-modal");
+      simpleModal.addEventListener("show.bs.modal", (e) => {
+          const bigImage = e.relatedTarget.getAttribute('data-bigimage')
+          img.src = bigImage;
+      });
+    });
+  })(document);
